f07d57eb611016cee5db102bb2b9901752e3ec8c
Mark Menger <m.menger@f5.com>
2019-09-11 09:12:14 -0700

    find a google bigip image

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,1 +1,8 @@
 # main GCP BIGIP provisioning module
+
+# information found at 
+# https://github.com/F5Networks/f5-google-gdm-templates/blob/master/supported/standalone/1nic/
+data "google_compute_image" "my_image" {
+  name      = "f5-bigip-14-1-0-3-0-0-6-payg-best-5gbps-190329165642"
+  project   = "f5-7626-networks-public" # f5 public project
+}
